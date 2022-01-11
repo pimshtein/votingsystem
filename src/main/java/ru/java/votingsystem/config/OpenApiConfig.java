95bb08c7bad7b0933a7c0b238b6d7a8bd15bf881
pimshtein <pimshtein@gmail.com>
2022-01-11 09:38:13 +0300

    Changed open api titles

--- src/main/java/ru/java/votingsystem/config/OpenApiConfig.java
+++ src/main/java/ru/java/votingsystem/config/OpenApiConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//https://sabljakovich.medium.com/adding-basic-auth-authorization-option-to-openapi-swagger-documentation-java-spring-95abbede27e9
 @SecurityScheme(
         name = "basicAuth",
         type = SecuritySchemeType.HTTP,
@@ -21,7 +20,7 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(
                 title = "REST API documentation",
                 version = "1.0",
-                description = "Приложение по <a href='https://javaops.ru/view/topjava'>для голосования</a>",
+                description = "<a href='https://github.com/pimshtein/votingsystem'>Приложение для голосования меню ресторана</a>",
                 contact = @Contact(url = "https://github.com/pimshtein", name = "Valentin Spiridonov", email = "pimshtein@gmail.com")
         ),
         security = @SecurityRequirement(name = "basicAuth")
